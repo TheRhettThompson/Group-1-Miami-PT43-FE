bf6bc8962f3f241716a2b49a929f04891d7e9d8f
betolix <betolix@gmail.com>
2023-01-28 00:43:12 +0000

    Routes working

--- react-career_space/src/App.js
+++ react-career_space/src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
 import React from 'react';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Navbar from './components/navbar';
-import Home from './components/home';
+import Home from './views/home';
 import Footer from './components/footer';
 import Landing from './views/landing';
+import Register from './views/register';
+import Login from './views/login';
+import AboutUs from './views/aboutUs';
 
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <Navbar/>
-        {/* add react routes here*/}
-        <Main/>
-        <Footer/>
+     <BrowserRouter>
+      <Navbar />
         
-     
-        
-      </header>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/aboutus" element={<AboutUs />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/landing" element={<Landing />} />
+
+        </Routes>
+      <Footer />
+      </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ react-career_space/src/views/aboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function AboutUs() {
+  return (
+    <div className="AboutUs">
+        <h1>This is the About Us page</h1>
+    </div>
+  )
+}
+
+export default AboutUs
+
+// Home page is totally separate page. 
