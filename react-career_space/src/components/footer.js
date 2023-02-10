dc58b1cce1ac3ccc08b977d5c02db90b568edba1
Christopher Morejon <Christophermorejon17@gmail.com>
2023-02-10 04:06:02 +0000

    seperated Nav and Footer CSS

--- react-career_space/src/components/footer.js
+++ react-career_space/src/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../App.css";
+import "../styles/footer.css";
 
 function Footer() {
   return (
--- react-career_space/src/components/navbar.jsx
+++ react-career_space/src/components/navbar.jsx
@@ -2,9 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
-import "../App.css";
-// import { Table } from "react-bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/navbar.css";
 
 function NavbarComponent() {
   return (
