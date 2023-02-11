d072cfdb7eef4b17c84da84785845f6d7833465a
TheRhettThompson <rthompsoncareer@gmail.com>
2023-02-11 02:04:03 +0000

    updated AboutUs

--- react-career_space/src/views/aboutUs.js
+++ react-career_space/src/views/aboutUs.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import "../styles/landing.css"
 
 
 function AboutUs() {
   return (
     <div className="AboutUs">
         <h1>This is the About Us page</h1>
+        <p>This is going to be information about CareerSpace. This is going to be the mission statement of CareerSpace</p>
+        <h1>This is going to be about the 3 of us creators</h1>
+        
+
+
     </div>
   )
 }
 
 export default AboutUs
 
-// Home page is totally separate page. 
