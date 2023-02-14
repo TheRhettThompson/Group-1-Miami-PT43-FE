f3030cf91dae66863bc95cbea9146ee3e1661017
TheRhettThompson <rthompsoncareer@gmail.com>
2023-02-14 22:48:45 +0000

    asset logos, new photos to landing

--- react-career_space/src/views/landing.js
+++ react-career_space/src/views/landing.js
@@ -9,6 +9,9 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import ryderLogo from '../assets/ryder-system-inc-logo-vector.png';
+import chewyLogo from '../assets/chewy_logo.png';
+import citadelLogo from '../assets/citadel_securities.png';
+import neorisLogo from '../assets/neoris_logo.png';
 
 
 
@@ -50,9 +53,7 @@ function Landing() {
 
           <Carousel.Caption>
             <h3>Why work in Silicon Valley when you could have endless beaches?</h3>
-            <p>
-              Search for tech jobs in Miami and South Florida here.
-            </p>
+            <p>Search for tech jobs in Miami and South Florida here.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -113,16 +114,25 @@ function Landing() {
       </div>
 
       <ButtonGroup aria-label="Companies" size="lg" className="mb-2">
-        {/* <Button variant="light" img src={require("../assets/LorettaMayumiThompsonPhoto.png")} /> */}
-        <Button variant="light"><a href='https://ryder.com/careers' target='_blank' ><img className='companyLogos' src={ryderLogo} /> </a>   </Button>
-        <Button variant="light">Chewy Logo</Button>
-        <Button variant="light">Citadel Logo</Button>
-        <Button variant="light">Brightstar Logo</Button>
-        <Button variant="light">TracFone Wireless Logo</Button>
-        <Button variant="light">ClearSale Logo</Button>
-        <Button variant="light">goTRG Logo</Button>
-        <Button variant="light">Mi9 Retail Logo</Button>
-        <Button variant="light">OpenEnglish Logo</Button>
+        <Button variant="light"><a href='https://ryder.com/careers' target='_blank' >
+          <img className='companyLogos' src={ryderLogo} /> </a>   
+        </Button>
+
+        <Button variant="light"><a href='https://careers.chewy.com/us/en/technology-and-product-careers' target='_blank'>
+          <img className='companyLogos' src={chewyLogo}/> </a> 
+        </Button>
+
+        <Button variant="light"><a href='https://www.citadelsecurities.com/careers/open-opportunities/' target='_blank'>
+          <img className='companyLogos' src={citadelLogo}/></a>
+        </Button>
+
+        <Button variant="light"><a href='https://careers.neoris.com/' target='_blank'>
+          <img className='companyLogos' src={neorisLogo}/></a>
+        </Button>
+
+        <Button variant="light">Some Logo</Button>
+        <Button variant="light">Another Logo</Button>
+        <Button variant="light">Another Logo</Button>
       </ButtonGroup>
 
       <Card>
@@ -140,42 +150,34 @@ function Landing() {
         </Card.Body>
       </Card>
 
-
-     
-
       <div>
         <div style={{display: 'flex'}}>
           <Card className='testimonyCard'>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1573164574001-518958d9baa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJsYWNrJTIwd29tYW4lMjB3b3JraW5nJTIwb24lMjBsYXB0b3B8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60/100px160" />
             <Card.Body>
-              <Card.Title>I love</Card.Title>
+              <Card.Title>Be a pioneer</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              South Florida is on its way to becoming the East Coast Silicon Valley. 
+              I want to be a part of it, and Career Space makes it easy!
               </Card.Text>
             </Card.Body>
           </Card>
 
           <Card className='testimonyCard'>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1635768229592-8c2532d33cb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjQ1fHx3b3JraW5nJTIwb24lMjBjb21wdXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60/100px160" />
             <Card.Body>
-              <Card.Title>I haTE</Card.Title>
+              <Card.Title>Right in my backyard</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                I thought I'd have to leave Miami to get a job in coding.
+                But not anymore, with Career Space I know the opportunities are right here!
               </Card.Text>
             </Card.Body>
           </Card>
-
         </div>
-
-
 
         <div style={{display: 'flex'}}>
           <Card className='testimonyCard'>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1613324996029-f6190a17838f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzkwfHx3b3JraW5nJTIwb24lMjBjb21wdXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60/100px160" />
             <Card.Body>
               <Card.Title>I SO SO</Card.Title>
               <Card.Text>
@@ -185,124 +187,20 @@ function Landing() {
               </Card.Text>
             </Card.Body>
           </Card>
-
+          
           <Card className='testimonyCard'>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src="https://images.unsplash.com/photo-1590650153855-d9e808231d41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHdvcmtpbmclMjBvbiUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60/100px160" />
             <Card.Body>
-              <Card.Title>I MEH</Card.Title>
+              <Card.Title>The best jobs for the best talent</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              South Florida is by far one of the best places for tech professionals 
+              to develop careers. Career Space is the best tool to do that!
               </Card.Text>
             </Card.Body>
           </Card>
         </div>
-
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <div className='cardGroup'>
-        <Card>
-          {/* <Card.Header>Testimonials</Card.Header>
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-                {' '}
-                I thought I'd have to leave Miami to get a job in coding.
-                But not anymore, with Career Space I know the opportunities are right here!{' '}
-              </p>
-              <footer className="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </Card.Body> */}
-        </Card>
-
-
-
-        {/* <Figure>
-          <Figure.Image
-            width={171}
-            height={180}
-            alt="371x380"
-            src="https://media.istockphoto.com/id/1212615578/photo/overweight-mature-latin-woman-doing-freelance-work-outdoors-on-laptop.jpg?s=612x612&w=0&k=20&c=69Ttt9FhyKwgwTK8QB_S0pf0149YmVad9j33ChfFcrs="
-          />
-          <Figure.Caption style={{color: "red", marginRight: "50px" , backgroundColor: "white"}}> 
-            South Florida is on its way to becoming the East Coast Silicon Valley. I want to be a part of it, and Career Space makes it easy!
-          </Figure.Caption>
-        </Figure> */}
-
-
-
-        {/* <Card>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1573164574001-518958d9baa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" />
-          <Card.Body>
-            <Card.Text>
-              South Florida is by far one of the best places for tech professionals to develop careers.
-              Career Space is the best tool to do that!
-            </Card.Text>
-          </Card.Body>
-        </Card> */}
-
-
-
-        {/* <Card>
-          <Card.Header>Quote</Card.Header>
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-                {' '}
-                I thought I'd have to leave Miami to get a job in coding.
-                But not anymore, with Career Space I know the opportunities are right here!{' '}
-              </p>
-              <footer className="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </Card.Body>
-        </Card> */}
-        {/* 
-        <br />
-        <Card>
-          <Card.Img variant="top" src="https://www.istockphoto.com/search/2/image?phrase=computer%20palm%20tree&alloweduse=availableforalluses&excludenudity=true&sort=best&mediatype=photography&utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcomputer-palm-tree&utm_term=computer%20palm%20tree%3A%3A%3A" />
-          <Card.Body>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br /> */}
-
-        {/* <Card>
-          <Card.Header>Quote</Card.Header>
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-                {' '}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                posuere erat a ante.{' '}
-              </p>
-              <footer className="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </Card.Body>
-        </Card> */}
-      </div>
-
+      
       <ButtonGroup aria-label="SocialMedia" size="lg" className="mb-2">
         <Button variant="backGroundColor= #8e793e"><i class="fa-brands fa-linkedin"></i></Button>
         <Button variant="backGroundColor= #8e793e"><i class="fa-brands fa-github"></i></Button>
@@ -312,7 +210,6 @@ function Landing() {
 
     </div>
   )
-
 }
 
 export default Landing
