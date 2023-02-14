76f1f27ede3d88ec145f91cb3bb2aba34fa06017
TheRhettThompson <rthompsoncareer@gmail.com>
2023-02-14 00:48:36 +0000

    reworked ContactUs

--- react-career_space/src/views/ContactUs.js
+++ react-career_space/src/views/ContactUs.js
@@ -1,58 +1,18 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
 import "../styles/ContactUs.css"
 
   
 function ContactUs() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
-
-    <>
+    <div>
+    
     <h1>Looking for work?</h1>
       <Button variant="warning" onClick={handleShow}>
         Find your next tech job now
       </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Get tech jobs in your inbox</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="email@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="warning" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+    </div>
+     
+  )
 }
   
 export default ContactUs
